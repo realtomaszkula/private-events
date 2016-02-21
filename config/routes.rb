4a40aec5c519837eb39d7515c718b34d6f31fa8c
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-21 20:26:49 +0100

    Add session model, controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users, only: [:index, :new, :create, :show]
   resources :events, only: [:index, :new, :create, :show]
+  resources :sessions, only: [:new]
+  get 'login'   => 'sessions#new'
+  get 'logout'  => 'sessions#destroy'
   root 'events#index'
 end
