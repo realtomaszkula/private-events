6530e496bc53039e05654e72dcc1b8a6abfa1f05
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-21 23:14:28 +0100

    Event and User controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  resources :users, only: [:index, :new, :create, :show]
+  resources :users, only: [:new, :create]
   resources :events, only: [:index, :new, :create, :show]
   resources :sessions, only: [:new]
   get 'login'   => 'sessions#new'
