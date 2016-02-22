18bf1cb228700b52a392f18541b484b19c7e5aae
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-22 22:48:18 +0100

    Add upcoming and past events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    @upcoming_events = @events.upcoming
+    @past_events = @events.past
+
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,14 @@ class Event < ActiveRecord::Base
 
   has_many :invitations, :foreign_key => :attended_event_id
   has_many :attendees, :through => :invitations
+
+  def self.upcoming
+    self.where("date > ?", Time.now)
+  end
+
+  def self.past
+    self.where("date < ?", Time.now)
+  end
+
+
 end
