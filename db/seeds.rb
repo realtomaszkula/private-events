97f4d1966ba38e22e549f25bc682e18eb806a7cf
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-22 15:21:13 +0100

    Add seeds for hosted events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
   User.create(name: name, email: email)
 end
 
+User.all.each do |user|
+  date = Faker::Date.between(1.days.from_now, 1.year.from_now)
+  description = Faker::Lorem.paragraph
+
+  user.hosted_events.create(date: date, description: description )
+end
