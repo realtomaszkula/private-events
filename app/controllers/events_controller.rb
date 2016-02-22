3a8885c6ae5e5c8d1d95b1f75839e68d1592e6b6
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-22 15:16:43 +0100

    Add user seed, add new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:description)
+    params.require(:event).permit(:description, :host_id, :date)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
+10.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email
+
+  User.create(name: name, email: email)
+end
 
